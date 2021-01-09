cf212035ffa65b65ef4f3a0be16e955037596bcd
Samir Safaraliyev <safaraliyevs@gmail.com>
2021-01-10 00:13:34 +0400

    Add Day8 Tasks solution

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String numbers = "";
 
+        long before = System.currentTimeMillis();
+
+        for (int i = 0; i <= 20000; i++)
+            numbers += i + " ";
+
+        long after = System.currentTimeMillis();
+        System.out.println("Время выполнения цикла с классом String : " + (after - before));
+        System.out.println(numbers);
+
+        StringBuilder sb = new StringBuilder();
+        before = System.currentTimeMillis();
+
+        for (int i = 0; i <= 20000; i++)
+            sb.append(i).append(" ");
+
+        after = System.currentTimeMillis();
+        System.out.println("Время выполненя цикла с классом StringBuilder : " + (after - before));
+        System.out.println(sb);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -1,7 +1,10 @@
 package day8;
 
+
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boeing", 2000, 20, 30000);
+        System.out.println(airplane);
 
     }
 }
